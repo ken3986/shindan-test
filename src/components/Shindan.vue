54c02367205c01e2559e534a94d866907688d0e3
ken3986 <naka_ken8686@yahoo.co.jp>
2021-05-06 15:56:34 +0900

    初期状態＋vuetify

---
+++ src/components/Shindan.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>診断テストのテスト</h1>
+    {{ test }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      test: 'testtest'
+    }
+  }
+}
+</script>
